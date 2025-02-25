d097007fc5ca7922a22f5a04c659e3f252516148
Heber ochoa <heber76@hotmail.com>
2025-02-25 15:22:32 -0600

    Begin Repository Pattern

--- Orders/ORders.Frontend/Program.cs
+++ Orders/ORders.Frontend/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ORders.Frontend;
+using ORders.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+////builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7095//") });
+builder.Services.AddScoped<IRepository , Repository>();
 await builder.Build().RunAsync();
---
+++ Orders/ORders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+
+namespace ORders.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrato.";
+            }
+
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que estar lofueado para ejecutar esta operacion";
+            }
+
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "No tienes  permiso para ejecutar esta operacion";
+            }
+
+            return "Ha pcurrido un error inesperado";
+        }
+    }
+}
